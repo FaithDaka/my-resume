8e2294ea52628f65689045888e3f62206009f080
FaithDaka <faithbusigu8@gmail.com>
2022-08-18 11:20:23 +0300

    My Resume

--- forms/contact.php
+++ forms/contact.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
 if(isset($_POST ['submit-button'])){
   $name = $_REQUEST['name'];
   $email = $_REQUEST['email'];
